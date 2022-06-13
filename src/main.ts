67ee47679621a619b386980b640c388cf8367742
F9932436 Yago Telles Candeias <yago.telles@bb.com.br>
2022-06-13 17:50:03 -0300

    menu

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+export { }
+
+console.log(`Escolha uma das regiões do console:\n`,
+  `0. Brasil\n`,
+  `1. Norte\n`,
+  `2. Norderte\n`,
+  `3. Sudeste\n`,
+  `4. Sul\n`,
+  `5. Centro-Oeste\n`)
+
+const opcao = parseInt(String(prompt("Escolha uma das regiões do console")))
+
+switch (opcao) {
+  case 0:
+    //função com população filtrada
+    break;
+  case 1:
+
+    break;
+  case 2:
+
+    break;
+  case 3:
+
+    break;
+  case 4:
+
+    break;
+  case 5:
+
+    break;
+
+  default:
+    alert("Informe uma opção válida!")
+}
