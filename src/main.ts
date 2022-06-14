75b9392fb073438fa10d960400330bcdbc109d95
F9932436 Yago Telles Candeias <yago.telles@bb.com.br>
2022-06-14 14:44:35 -0300

    menu sem looping

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-/*import {getPopulacaoBrasileira, getRegiao} from '../utils/request';
-
+import {getPopulacaoBrasileira, getRegiao} from '../utils/request';
+/*
 getPopulacaoBrasileira().then(populacao =>{
     console.log(`Exemplo 1\nPopulação brasileira: ${populacao}`);
 }).catch(err =>{
@@ -10,40 +10,44 @@ getRegiao(1).then(regiao =>{
     console.log(`Exemplo 2\nPopulação região norte: ${regiao}`);
 })
 */
-
-
-export { }
-
-console.log(`Escolha uma das regiões do console:\n`,
-  `0. Brasil\n`,
-  `1. Norte\n`,
-  `2. Norderte\n`,
-  `3. Sudeste\n`,
-  `4. Sul\n`,
-  `5. Centro-Oeste\n`)
+const regioes = {
+  1:'Norte',
+  2:'Nordeste',
+  3:'Sudeste',
+  4:'Sul',
+  5:'Centro-Oeste'
+}
 
 const opcao = parseInt(String(prompt("Escolha uma das regiões do console")))
 
-switch (opcao) {
-  case 0:
-    //função com população filtrada
-    break;
-  case 1:
+telaInicial(opcao)
 
-    break;
-  case 2:
+function telaInicial(input: number){
 
-    break;
-  case 3:
-
-    break;
-  case 4:
-
-    break;
-  case 5:
-
-    break;
-
-  default:
-    alert("Informe uma opção válida!")
+  switch (input) {
+    case 0:
+      getPopulacaoBrasileira().then(populacao =>{
+        console.log('-------------------------------------')
+        console.log(`População brasileira: ${populacao}`);
+        console.log('-------------------------------------')
+    }).catch(err =>{
+        console.log(err);
+    });
+      break;
+  
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      getRegiao(input).then(regiao =>{
+        console.log('-------------------------------------')
+        console.log(`População da região ${regioes[input]}: ${regiao}`);
+        console.log('-------------------------------------')
+    })
+      break;
+    default:
+      alert("Informe uma opção válida!")
+  }
 }
+
